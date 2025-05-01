559299626a22d23fd1c32044107518d3b8313988
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-05-01 13:53:59 +0530

    [Sanya] Add: Security Config

--- src/main/java/com/cg/budgetboard/config/SecurityConfig.java
+++ src/main/java/com/cg/budgetboard/config/SecurityConfig.java
@@ -1,13 +1,40 @@
 package com.cg.budgetboard.config;
 
 
+import com.cg.budgetboard.util.JwtAuthenticationEntryPoint;
+import com.cg.budgetboard.util.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    JwtAuthenticationFilter jwtRequestFilter;
+
+    @Autowired
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/user/register", "/user/login", "/", "/getAddress", "/addAddress", "/user/forget-password","/user/reset-password").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex-> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 }
