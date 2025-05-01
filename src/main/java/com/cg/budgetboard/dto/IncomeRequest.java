1905c35793dc89473e53e9fb6a170423b9dcc659
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-05-01 16:44:08 +0530

    [Sanya] Add: Income request class created

--- src/main/java/com/cg/budgetboard/dto/IncomeRequest.java
+++ src/main/java/com/cg/budgetboard/dto/IncomeRequest.java
@@ -1,4 +1,13 @@
 package com.cg.budgetboard.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class IncomeRequest {
+    private Double amount;
+    private LocalDate date;
+    private Long categoryId; // sending category by ID
 }
+
