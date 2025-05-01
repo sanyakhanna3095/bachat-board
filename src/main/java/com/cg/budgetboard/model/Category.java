cbc9c71667660e05464caa5c32998107c8998418
Hitesh0627 <103327522+Hitesh0627@users.noreply.github.com>
2025-05-01 15:16:22 +0530

    [Hitesh] ADD: Added Category model

---
+++ src/main/java/com/cg/budgetboard/model/Category.java
@@ -0,0 +1,22 @@
+package com.cg.budgetboard.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name; // e.g., Food, Rent
+    private String type; // EXPENSE or INCOME
+}
+
