62f0c0e9c4d2b0f1a4f60c248ac1fd4cf0a8d1a3
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-05-01 09:50:11 +0530

    [Sanya] Add: Git Initialised

---
+++ src/main/java/com/cg/budgetboard/BudgetBoardApplication.java
@@ -0,0 +1,13 @@
+package com.cg.budgetboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/budgetboard/BudgetBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.budgetboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
