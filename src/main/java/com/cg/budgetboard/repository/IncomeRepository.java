964f1f9c4d6040f999743fde0381a7693e546656
Manan Gupta <manangupta27.mg@gmail.com>
2025-05-03 12:02:11 +0530

    [Manan] ADD:Added query to fetch montly income

--- src/main/java/com/cg/budgetboard/repository/IncomeRepository.java
+++ src/main/java/com/cg/budgetboard/repository/IncomeRepository.java
@@ -3,6 +3,8 @@ package com.cg.budgetboard.repository;
 import com.cg.budgetboard.model.Income;
 import com.cg.budgetboard.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,5 +15,6 @@ import java.util.Optional;
 public interface IncomeRepository extends JpaRepository<Income, Long> {
     List<Income> findByUser(User user);
 
-    List<Income> findByUserAndDateBetween(User user, LocalDate startDate, LocalDate endDate);
+    @Query("SELECT i FROM Income i WHERE i.user.id = :userId AND FUNCTION('DATE_FORMAT', i.date, '%m-%Y') = :month")
+    List<Income> findByUserAndMonth(@Param("userId") Long userId, @Param("month") String month);
 }
