dd311024991f9b0927a05847d6fe9d063e64487f
iharshit766 <harshi766766@gmail.com>
2025-05-03 11:47:13 +0530

    [Harshit] ADD: Added banner functionality and 70% and 90% alert.

--- src/main/java/com/cg/budgetboard/services/ExpenseService.java
+++ src/main/java/com/cg/budgetboard/services/ExpenseService.java
@@ -28,7 +28,7 @@ public class ExpenseService {
     private final AuthUtil authUtil;
     private final EmailService emailService;
 
-    public Expense addExpense(ExpenseRequestDTO dto, String token) {
+    public void addExpense(ExpenseRequestDTO dto, String token) {
         User user = authUtil.getCurrentUser(token);
         Category category = categoryRepository.findById(dto.getCategoryId())
                 .orElseThrow(() -> new CustomException("Category not found"));
@@ -37,21 +37,26 @@ public class ExpenseService {
             throw new CustomException("Category is not of type EXPENSE");
         }
 
-        // Check if budget exists for the user and category
         String expenseMonth = dto.getDate().format(DateTimeFormatter.ofPattern("MM-yyyy"));
-
         Budget budget = budgetRepository.findByUserIdAndCategoryIdAndMonth(user.getId(), category.getId(), expenseMonth)
                 .orElseThrow(() -> new RuntimeException("No budget set for this category in " + expenseMonth));
-        // Calculate total expenses for the category
         Double totalExpenses = expenseRepository.getTotalExpenseByUserAndCategory(user.getId(), category.getId());
 
         double newTotal = totalExpenses + dto.getAmount();
         double budgetLimit = budget.getAmount();
 
-        if (newTotal > 0.7 * budgetLimit) {
-            emailService.sendEmail(user.getEmail(), "Expense Alert",
-                    "You've exceeded 70% of your allotted budget for category: " + category.getName());
+        if (newTotal > 0.9 * budgetLimit) {
+            String percentage = "90%";
+            emailService.sendAlertEmail(user.getEmail(), percentage,
+                    category.getName());
         }
+
+        else if (newTotal > 0.7 * budgetLimit) {
+            String percentage = "70%";
+            emailService.sendAlertEmail(user.getEmail(), percentage,
+                    category.getName());
+        }
+
 
         Expense expense = Expense.builder()
                 .amount(dto.getAmount())
@@ -61,7 +66,7 @@ public class ExpenseService {
                 .category(category)
                 .build();
 
-        return expenseRepository.save(expense);
+        expenseRepository.save(expense);
     }
 
     public List<ExpenseResponseDTO> getAllExpenses(String token) {
@@ -107,17 +112,6 @@ public class ExpenseService {
                 updatedExpense.getDate(),
                 updatedExpense.getCategory().getName()
         );
-    }
-    public void deleteExpense(Long id, String token) {
-        User user = authUtil.getCurrentUser(token);
-        Expense expense = expenseRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Expense not found"));
-
-        if (!expense.getUser().getId().equals(user.getId())) {
-            throw new RuntimeException("Unauthorized");
-        }
-
-        expenseRepository.delete(expense);
     }
 }
 
