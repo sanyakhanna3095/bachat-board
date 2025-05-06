a74d6409ad0dc51f4b39f9f7d911d80f29db8684
Manan Gupta <manangupta27.mg@gmail.com>
2025-05-06 09:48:25 +0530

    [Manan] MOD: Modified response body

--- src/main/java/com/cg/budgetboard/dto/ResponseDTO.java
+++ src/main/java/com/cg/budgetboard/dto/ResponseDTO.java
@@ -1,11 +1,14 @@
 package com.cg.budgetboard.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public @Data class ResponseDTO<S, S1> {
     private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Object data;
 
     public ResponseDTO(String message, Object data) {
